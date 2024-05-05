353e6c83fa9af2b167ab6989c718131185459dc1
Arseny Boykov <36469655+Bobronium@users.noreply.github.com>
2024-05-05 14:27:45 +0200

    Initial Commit

---
+++ Switcher/SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitcherApp.swift
+//  Switcher
+//
+//  Created by Bobronium on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
